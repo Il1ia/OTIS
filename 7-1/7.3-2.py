031150bb941f9b97085b6716d99a5b5f1631f90c
Клименко Илья 1-16 Фіт <113426961+Il1ia@users.noreply.github.com>
2022-09-20 09:23:14 +0300

    Add files via upload

---
+++ 7-1/7.3-2.py
@@ -0,0 +1,22 @@
+import math
+x1=float(input("значення x1"))
+x2=float(input("значення x2"))
+x3=float(input("значення x3"))
+a=float(input("значення "))
+if x1%a==0 :
+ if x2%a==0 :
+  if x3%a==0 :
+    print("Є ділтником всіх чисел")
+  else:
+    print("не є дільником всіх чисел")
+ else:
+  print("не є дільником всіх чисел")
+else:
+ print("не є дільником всіх чисел")
+
+
+
+
+
+
+
